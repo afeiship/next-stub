12d8d686d17dc32a39e4eb18bd207574539acb09
afeiship <afeiship@gmail.com>
2018-12-18 22:37:52 +0800

    feat: add export

--- src/next-stub.js
+++ src/next-stub.js
@@ -1,39 +1,45 @@
-(function () {
-
+(function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
   var RETURN_THEN = { then: nx.noop };
 
-  nx.stubString = function(){
+  nx.stubString = function() {
     return '';
   };
 
-  nx.stubArray = function(){
+  nx.stubArray = function() {
     return [];
   };
 
-  nx.stubObject = function(){
+  nx.stubObject = function() {
     return {};
   };
 
-  nx.stubValue = function (inValue) {
+  nx.stubValue = function(inValue) {
     return inValue;
   };
 
-  nx.stubTrue = function () {
+  nx.stubTrue = function() {
     return true;
   };
 
-  nx.stubFalse = function () {
+  nx.stubFalse = function() {
     return false;
   };
 
-  nx.stubThen = function () {
+  nx.stubThen = function() {
     return RETURN_THEN;
   };
 
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.stub;
+    module.exports = {
+      stubString: nx.stubString,
+      stubArray: nx.stubArray,
+      stubObject: nx.stubObject,
+      stubValue: nx.stubValue,
+      stubTrue: nx.stubTrue,
+      stubFalse: nx.stubFalse,
+      stubThen: nx.stubThen
+    };
   }
-
-}());
+})();
